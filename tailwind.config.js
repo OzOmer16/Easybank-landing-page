9f9f33139542b9f8059d8607ce003dcd7c37e05a
Omer <iitw133@gmail.com>
2022-04-24 23:36:51 +0300

    Configure Tailwind and create navbar

---
+++ tailwind.config.js
@@ -0,0 +1,37 @@
+module.exports = {
+  mode: 'jit',
+  content: [
+    './**/*.html',
+    './**/*.{js,jsx,ts,tsx,vue}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primaryDarkBlue: 'hsl(233, 26%, 24%)',
+        primaryLimeGreen: 'hsl(136, 65%, 51%)',
+        primaryBrightCyan: 'hsl(192, 70%, 51%)',
+        neutralGrayishBlue: 'hsl(233, 8%, 62%)',
+        neutralLightGrayishBlue: 'hsl(220, 16%, 96%)',
+        neutralVeryLightGray: 'hsl(0, 0%, 98%)',
+        neutralWhite: 'hsl(0, 0%, 100%)'
+
+      },
+      fontFamily: {
+        publicSans: ['Public Sans', 'sans-serif']
+      },
+      screens: {
+        customMobile: { 'raw': '(max-width: 375px)' },
+        container: { 'raw': '(max-width: 1152px)' },
+        customDesktop: { 'raw': '(max-width: 1440px)' }
+      }
+      
+    },
+  },
+  plugins: [],
+}
+
+
+
+
+
+
